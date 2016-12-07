73b77796b3338f89d08172ee5be7d14f4e80cecd
YosukeSasaoka <yosuke.sasaoka@nextremer.com>
2016-12-07 16:36:38 +0900

    react-routerに変更

--- src/js/app.js
+++ src/js/app.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Main from './components/Main';
+import { Router, Route, hashHistory } from 'react-router';
 
-ReactDOM.render(
-  <Main />,
-  document.getElementById('main')
+ReactDOM.render((
+  <Router history={hashHistory}>
+    <Route path="/" component={Main} />
+  </Router>
+), document.getElementById('main')
 );
 
