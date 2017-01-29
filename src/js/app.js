53ce784cb46e4b4ad74675a888f6a366ff4b3fad
MitsuhiroKamei <m_kamei_2501@yahoo.co.jp>
2017-01-30 06:26:18 +0900

    とりあえず動作した

--- src/js/app.js
+++ src/js/app.js
@@ -6,13 +6,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { createStore, combineReducers } from 'redux'
 import { Provider } from 'react-redux'
-import { Router, Route, IndexRoute, browserHistory } from 'react-router';
+import { Router, Route, browserHistory } from 'react-router';
 import { syncHistoryWithStore, routerReducer } from 'react-router-redux'
 
 import * as reducers from './reducers'
 import Todo from './components/Todo'
 import User from './components/User'
-import TodoApp from './containers/TodoApp'
 
 const reducer = combineReducers({
   ...reducers,
@@ -34,7 +33,14 @@ const history = syncHistoryWithStore(browserHistory, store)
 
 ReactDOM.render(
   <Provider store={store}>
-    <TodoApp />
+    <div>
+      <Router history={history}>
+        <Route path="/" component={User}/>
+        <Route path="User" component={User}/>
+        <Route path="Todo" component={Todo}/>
+      </Router>
+      <DevTools />
+    </div>
   </Provider>, 
   document.getElementById('main')
 );
--- src/js/reducers/index.js
+++ src/js/reducers/index.js
@@ -1,8 +1,1 @@
-import { combineReducers } from 'redux'
-import todos from './todos'
-
-const rootReducer = combineReducers({
-  todos
-})
-
-export default rootReducer
+export todos from './todos'
